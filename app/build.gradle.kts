0f25ad264f7848895d1938e1060347a091a1b0ad
zabio3 <t.kusumoto@gogen.jp>
2026-01-25 19:13:26 +0900

    Bump versionCode to 2 for Google Play release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ethersense"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
